e45aa36d74a554b9d6dbff80d1f303d8115cffe2
DeepikaPadam <manideepika.padam@gmail.com>
2020-10-30 16:01:05 +0530

    Update search.component.spec.ts

--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,14 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {DisplayDetailsComponent} from '../display-details/display-details.component';
 
-xdescribe('SearchComponent', () => {
+describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+       imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'details',
+            component: DisplayDetailsComponent
+          }
+        ]),
+      ],
+      declarations: [
+        SearchComponent,
+        DisplayDetailsComponent
+      ]  
     })
     .compileComponents();
   }));
@@ -19,7 +34,7 @@ xdescribe('SearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create search component', () => {
     expect(component).toBeTruthy();
   });
 });
